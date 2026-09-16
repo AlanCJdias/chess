0d8b62e2f3b7e53eb83db6ac4d1a2bc8a9b45435
Alan Cristian <alan.dias@dcomp.ufs.br>
2026-09-15 21:15:53 -0300

    Adicionando um sistema de pontuação para os jogadores.

--- tabuleiroTela/Tabuleiro.java
+++ tabuleiroTela/Tabuleiro.java
@@ -7,11 +7,13 @@ public class Tabuleiro{
     //preciso fazer uma matriz de 8X8
     //são atributas da classe inteira.
 
-    public  pecas[][] tabuleiro;//aqui vai ficar todsa as informações das peças.
-    public  boolean acabou;
-    public  int quantidade_pecas_pretas;
-    public  int quantidade_pecas_brancas;
-    public  boolean turnoBranco;//as brancas sempre começam jogando.
+    public pecas[][] tabuleiro;//aqui vai ficar todsa as informações das peças.
+    public boolean acabou;
+    public int quantidade_pecas_pretas;
+    public int quantidade_pecas_brancas;
+    public boolean turnoBranco;//as brancas sempre começam jogando.
+    public int pontosBrancos;
+    public int pontosPretos;
 
     public Tabuleiro(){//cria o tabuleiro.
         this.tabuleiro = inicialização.preencherTabuleiro();
@@ -19,15 +21,8 @@ public class Tabuleiro{
         this.quantidade_pecas_brancas = 16;
         this.quantidade_pecas_pretas = 16;
         this.turnoBranco = true;
+        this.pontosBrancos = 0;
+        this.pontosPretos = 0;
         }
     
-    public static void mover(){
-
-        //ele deve escolher uma peça com o mouse
-        //indicar uma casa que para a peça ir. o jogo deve indicar se é possível.
-        //deve indicar se tem uma peça inimiga.
-
-
-
-    }
 }
--- tabuleiroTela/movimentação.java
+++ tabuleiroTela/movimentação.java
@@ -1,8 +1,14 @@
 package tabuleiroTela;
 
+import peices.estrutura.pecas;
+
 public class movimentação {
 
-    public static void mover(){
+    public static void mover(
+        pecas atual,//escolhe a peça.
+    ){
+
+        //vai pegar a peça, ver onde você quer colocar, vê se tem ume peça lá.
 
     }
 }
